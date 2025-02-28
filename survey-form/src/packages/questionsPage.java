6c3e75ee1edcbacb196900077e60720072f7f9a8
OiliaO <s.iliarabee2006@gmail.com>
2025-02-28 17:37:05 +0330

    translate text to English

--- survey-form/src/packages/questionsPage.java
+++ survey-form/src/packages/questionsPage.java
@@ -11,7 +11,7 @@ public class questionsPage {
         panel.setLayout(null);
 
         // Question 1
-        JLabel question1 = new JLabel("چقدر رابط کاربری نرم افزار از نظر شما جذاب است؟");
+        JLabel question1 = new JLabel("How attractive is the software's user interface to you?");
         question1.setBounds(10, 10, 300, 20);
 
         JRadioButton radio1_1 = new JRadioButton("۱");
@@ -33,7 +33,7 @@ public class questionsPage {
         group1.add(radio1_5);
 
         // Question 2
-        JLabel question2 = new JLabel("آیا دسترسی به امکانات مختلف نرم افزار برای شما آسان است؟");
+        JLabel question2 = new JLabel("Is it easy for you to access the various features of the software?");
         question2.setBounds(10, 90, 300, 20);
 
 
@@ -56,7 +56,7 @@ public class questionsPage {
         group2.add(radio2_5);
 
         // Question 3
-        JLabel question3 = new JLabel("چقدر طراحی صفحات نرم افزار ساده و کاربرپسند است؟");
+        JLabel question3 = new JLabel("How simple and user-friendly is the software's page design?");
         question3.setBounds(10, 170, 300, 20);
 
         JRadioButton radio3_1 = new JRadioButton("۱");
@@ -78,7 +78,7 @@ public class questionsPage {
         group3.add(radio3_5);
 
         // Question 4
-        JLabel question4 = new JLabel("آیا رنگ ها و فونت های استفاده شده در نرم افزار مناسب هستند؟");
+        JLabel question4 = new JLabel("Are the colors and fonts used in the software appropriate?");
         question4.setBounds(10, 250, 350, 20);
 
         JRadioButton radio4_1 = new JRadioButton("۱");
@@ -100,7 +100,7 @@ public class questionsPage {
         group4.add(radio4_5);
 
         // Next button to go to Page3
-        JButton nextButton = new JButton("صفحه بعد");
+        JButton nextButton = new JButton("Next Page");
         nextButton.setBounds(10, 320, 100, 20);
         nextButton.addActionListener(e -> {
             frame.remove(panel);
