ca9b29d9c22ba86a3df4f2dfaa2e5bfeea0564f8
OiliaO <s.iliarabee2006@gmail.com>
2025-02-28 18:33:18 +0330

    change numbers to English

--- survey-form/src/packages/questionsPage.java
+++ survey-form/src/packages/questionsPage.java
@@ -14,15 +14,15 @@ public class questionsPage {
         JLabel question1 = new JLabel("How attractive is the software's user interface to you?");
         question1.setBounds(10, 10, 300, 20);
 
-        JRadioButton radio1_1 = new JRadioButton("۱");
+        JRadioButton radio1_1 = new JRadioButton("1");
         radio1_1.setBounds(10, 40, 100, 20);
-        JRadioButton radio1_2 = new JRadioButton("۲");
+        JRadioButton radio1_2 = new JRadioButton("2");
         radio1_2.setBounds(80, 40, 100, 20);
-        JRadioButton radio1_3 = new JRadioButton("۳");
+        JRadioButton radio1_3 = new JRadioButton("3");
         radio1_3.setBounds(150, 40, 100, 20);
-        JRadioButton radio1_4 = new JRadioButton("۴");
+        JRadioButton radio1_4 = new JRadioButton("4");
         radio1_4.setBounds(220, 40, 100, 20);
-        JRadioButton radio1_5 = new JRadioButton("۵");
+        JRadioButton radio1_5 = new JRadioButton("5");
         radio1_5.setBounds(290, 40, 100, 20);
 
         ButtonGroup group1 = new ButtonGroup();
@@ -37,15 +37,15 @@ public class questionsPage {
         question2.setBounds(10, 90, 300, 20);
 
 
-        JRadioButton radio2_1 = new JRadioButton("۱");
+        JRadioButton radio2_1 = new JRadioButton("1");
         radio2_1.setBounds(10, 120, 100, 20);
-        JRadioButton radio2_2 = new JRadioButton("۲");
+        JRadioButton radio2_2 = new JRadioButton("2");
         radio2_2.setBounds(80, 120, 100, 20);
-        JRadioButton radio2_3 = new JRadioButton("۳");
+        JRadioButton radio2_3 = new JRadioButton("3");
         radio2_3.setBounds(150, 120, 100, 20);
-        JRadioButton radio2_4 = new JRadioButton("۴");
+        JRadioButton radio2_4 = new JRadioButton("4");
         radio2_4.setBounds(220, 120, 100, 20);
-        JRadioButton radio2_5 = new JRadioButton("۵");
+        JRadioButton radio2_5 = new JRadioButton("5");
         radio2_5.setBounds(290, 120, 100, 20);
 
         ButtonGroup group2 = new ButtonGroup();
@@ -59,15 +59,15 @@ public class questionsPage {
         JLabel question3 = new JLabel("How simple and user-friendly is the software's page design?");
         question3.setBounds(10, 170, 300, 20);
 
-        JRadioButton radio3_1 = new JRadioButton("۱");
+        JRadioButton radio3_1 = new JRadioButton("1");
         radio3_1.setBounds(10, 200, 100, 20);
-        JRadioButton radio3_2 = new JRadioButton("۲");
+        JRadioButton radio3_2 = new JRadioButton("2");
         radio3_2.setBounds(80, 200, 100, 20);
-        JRadioButton radio3_3 = new JRadioButton("۳");
+        JRadioButton radio3_3 = new JRadioButton("3");
         radio3_3.setBounds(150, 200, 100, 20);
-        JRadioButton radio3_4 = new JRadioButton("۴");
+        JRadioButton radio3_4 = new JRadioButton("4");
         radio3_4.setBounds(220, 200, 100, 20);
-        JRadioButton radio3_5 = new JRadioButton("۵");
+        JRadioButton radio3_5 = new JRadioButton("5");
         radio3_5.setBounds(290, 200, 100, 20);
 
         ButtonGroup group3 = new ButtonGroup();
@@ -81,15 +81,15 @@ public class questionsPage {
         JLabel question4 = new JLabel("Are the colors and fonts used in the software appropriate?");
         question4.setBounds(10, 250, 350, 20);
 
-        JRadioButton radio4_1 = new JRadioButton("۱");
+        JRadioButton radio4_1 = new JRadioButton("1");
         radio4_1.setBounds(10, 280, 100, 20);
-        JRadioButton radio4_2 = new JRadioButton("۲");
+        JRadioButton radio4_2 = new JRadioButton("2");
         radio4_2.setBounds(80, 280, 100, 20);
-        JRadioButton radio4_3 = new JRadioButton("۳");
+        JRadioButton radio4_3 = new JRadioButton("3");
         radio4_3.setBounds(150, 280, 100, 20);
-        JRadioButton radio4_4 = new JRadioButton("۴");
+        JRadioButton radio4_4 = new JRadioButton("4");
         radio4_4.setBounds(220, 280, 100, 20);
-        JRadioButton radio4_5 = new JRadioButton("۵");
+        JRadioButton radio4_5 = new JRadioButton("5");
         radio4_5.setBounds(290, 280, 100, 20);
 
         ButtonGroup group4 = new ButtonGroup();
